a5cc599a9fe14ebeec3cc3a1462adfc268e210b1
Viduality <38223961+Viduality@users.noreply.github.com>
2018-10-06 20:52:17 +0200

    Add files via upload

---
+++ SetHealth.java
@@ -0,0 +1,45 @@
+package com.github.Viduality.SetMaxHealth;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+/**
+ * Copyright (C) 2018 Viduality
+ * <p>
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Viduality
+ */
+
+public class SetHealth extends JavaPlugin implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("setmaxhealth")) {
+            if (args.length == 2) {
+                Player player = getServer().getPlayer(args[0]);
+                if (!(player == null)) {
+                    player.setMaxHealth(Double.parseDouble(args[1]));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
